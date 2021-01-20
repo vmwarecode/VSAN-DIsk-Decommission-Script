6aa6d1798b6cb1bb62b4a9b4c7e1837224ee157e
VMware Code <noreply@vmware.com>
2021-01-20 23:51:56 +0000

    Initial commit by Chen Wei

--- VSAN-DIsk-Decommission-Script.sh
+++ VSAN-DIsk-Decommission-Script.sh
@@ -1,4 +1,4 @@
-/bin/sh
+#!/bin/sh
 echo "Leaving the VSAN Cluster..."
 esxcli vsan cluster leave
 echo "Done"
@@ -7,23 +7,9 @@ echo "Disabling Auto Mode..."
 esxcli vsan storage automode set --enabled false
 echo "Done"
 
-
-esxcli vsan storage list > deviceList
-
-for i in `grep 'VSAN Disk Group Name' deviceList | sort -u | cut -d " " -f8`
+for i in `esxcli vsan storage list | grep "VSAN Disk Group Name:" | awk '{print $5}' | sort | uniq`
 do
   echo "Removing Disk Group..."
   esxcli vsan storage remove -s $i
   echo "Done"
 done
-
-
-for i in `grep 'Is SSD:' -n deviceList | cut -d ":" -f1`
-do
-  num=`expr $i - 3`
-  echo "Reseting Labels for all the disks..."
-  partedUtil mklabel /dev/disks/`sed "$num!d" deviceList` gpt
-  echo "Done"
-done
-
-rm -rf deviceList
